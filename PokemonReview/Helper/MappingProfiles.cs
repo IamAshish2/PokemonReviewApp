115e56c52ccc03c7c6ddda4783127237c5d32a6b
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-09 22:16:41 +0545

    added mapping for country and owner models

--- PokemonReview/Helper/MappingProfiles.cs
+++ PokemonReview/Helper/MappingProfiles.cs
@@ -11,7 +11,8 @@ namespace PokemonReview.Helper
         {
             CreateMap<Pokemon, PokemonDto>();
             CreateMap<Category, CategoryDto>();
-
+            CreateMap<Country,CountryDto>();
+            CreateMap<Owner,OwnerDto>();
         }
     }
 }
