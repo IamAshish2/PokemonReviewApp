753f934cb4988603940667e636bd6e9b9532072f
Ashish karki <134418831+IamAshish2@users.noreply.github.com>
2024-08-09 22:17:27 +0545

    addscoped for country and owner repo pattern

--- PokemonReview/Program.cs
+++ PokemonReview/Program.cs
@@ -13,8 +13,10 @@ builder.Services.AddTransient<Seed>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(options =>
@@ -49,6 +51,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-    app.MapControllers();
+app.MapControllers();
 
 app.Run();
